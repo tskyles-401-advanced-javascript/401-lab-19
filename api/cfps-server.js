2b8b7cbc2ad149698561d1acc0033d381aecd70f
travis skyles <travis.skyles@gmail.com>
2020-01-30 12:52:24 -0800

    initial api server setup

---
+++ api/cfps-server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3001');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/delivery/:retailer/:code', handleEvent);
+
+function postThing(req, res){
+  let payload = {
+    retailer: TODO, 
+    code: TODO,
+  };
+  socket.emit('package-delivered', payload);
+  res.status(200).send(payload);
+}
+
+app.listen(PORT, () => console.log(`api server listening on ${PORT}`));
