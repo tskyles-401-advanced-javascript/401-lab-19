0beb7d5373f916bc06e98d2d02f48ca5ba4a3fde
travis skyles <travis.skyles@gmail.com>
2020-01-31 08:18:58 -0800

    basic test scaffold

---
+++ __tests__/cfps-server.tests.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('cfps-server fuctionality', () => {
+
+  it('returns data when the route is hit', () => {
+    let test = 'pass';
+
+    expect(test).toBe('pass');
+  });
+});
